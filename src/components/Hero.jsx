6d91fccf31b8b39401d6591fa1a4d899fedbdbe8
VivekNagra <71701790+VivekNagra@users.noreply.github.com>
2025-10-09 22:52:22 +0200

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,7 @@ export default function Hero() {
             From dashboards to full-stack apps, my focus is on clarity, performance, and precision.
           </p>
           <br></br>
+          <br></br>
             <a href="#projects" className="link-underline rounded-md bg-[--color-brand] px-5 py-2.5 text-white shadow hover:opacity-90">
               View Projects
             </a>
