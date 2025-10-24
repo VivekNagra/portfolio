bc637b1fc18ddf0a2635cced19fc63a0942018a2
VivekNagra <71701790+VivekNagra@users.noreply.github.com>
2025-10-24 15:40:48 +0200

    added speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ScrollReveal from './components/ScrollReveal.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')).render(
       <App />
     </ScrollReveal>
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
